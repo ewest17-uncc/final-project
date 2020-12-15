66b164591ecb4cceadbbb05b11950dc78aca3de5
Evan West <ewest17@uncc.edu>
2020-12-15 22:47:21 +0000

    add react front end

---
+++ budget-app/src/components/HomePage/home.component.js
@@ -0,0 +1,45 @@
+import React, { Component }  from 'react';
+import UserService from "../../services/user.service";
+import { Link } from "react-router-dom";
+// import axios from 'axios';
+// import Chart from 'chart.js';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  componentDidMount() {
+    UserService.getPublicContent().then(
+      response => {
+        this.setState({
+          content: response.data
+        });
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString()
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header className="jumbotron">
+          <h3>
+            Welcome to my personal budget app!
+          </h3>
+        </header>
+        </div>
+    );
+  }
+}
